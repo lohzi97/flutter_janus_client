05522cad436bd298572762c82a922fd7573d31d1
lohzi97 <lohzian@yahoo.com>
2025-10-03 10:46:35 +0800

    Completed SipHangupEvent to hold call_id and sip.

--- lib/interfaces/sip/events/sip_hangup_event.dart
+++ lib/interfaces/sip/events/sip_hangup_event.dart
@@ -8,15 +8,39 @@
 part of janus_client;
 
 class SipHangupEvent {
+  String? sip;
+  String? callId;
+  SipHangupEventResult? result;
+
+  SipHangupEvent({this.sip, this.callId, this.result});
+
+  SipHangupEvent.fromJson(Map<String, dynamic> json) {
+    this.sip = json["sip"];
+    this.callId = json["call_id"];
+    this.result = json["result"] != null ? SipHangupEventResult.fromJson(json["result"]) : null;
+  }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data["sip"] = this.sip;
+    data["call_id"] = this.callId;
+    if (this.result != null) {
+      data["result"] = this.result!.toJson();
+    }
+    return data;
+  }
+  
+}
+
+class SipHangupEventResult {
   String? event;
   int? code;
   String? reason;
   String? reasonHeaderProtocol;
   String? reasonHeaderCause;
 
-  SipHangupEvent({this.event, this.code, this.reason, this.reasonHeaderProtocol, this.reasonHeaderCause});
+  SipHangupEventResult({this.event, this.code, this.reason, this.reasonHeaderProtocol, this.reasonHeaderCause});
 
-  SipHangupEvent.fromJson(Map<String, dynamic> json) {
+  SipHangupEventResult.fromJson(Map<String, dynamic> json) {
     this.event = json["event"];
     this.code = json["code"];
     this.reason = json["reason"];
